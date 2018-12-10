38b3258d602caa72af8fb21ccfa7905dea4f729e
Jon Jaques <jaquers@gmail.com>
2018-12-09 18:03:48 -0600

    Android build works

--- src/app/components/Application.tsx
+++ src/app/components/Application.tsx
@@ -3,19 +3,21 @@ import {View, Text, StyleSheet} from 'react-native'
 
 const styles = StyleSheet.create({
   root: {
-    textAlign: 'center',
-    fontWeight: 'bold',
     padding: 20,
-    height: '100vh',
-    backgroundColor: '#000',
-    color: '#fff'
+    height: '100%',
+    backgroundColor: '#000000'
+  },
+  text: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#ffffff'
   }
 })
 
 export class Application extends React.Component {
   render() {
     return <View style={styles.root}>
-      <Text>Nidavellir</Text>
+      <Text style={styles.text}>Nidavellir</Text>
     </View>
   }
 }
